da0a831282b2517ba2c67fece45151eb837ee97a
harry_kwak <harrykwak123@gmail.com>
2022-08-18 15:00:52 +0900

    feat: Add App.js with React-router-dom

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Post from "./pages/Post";
+import Main from "./pages/Main";
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/post" element={<Post />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

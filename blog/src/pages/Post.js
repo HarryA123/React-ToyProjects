186229de893be5fc52b6a9f52d73fd39fc7cb47d
harry_kwak <harrykwak123@gmail.com>
2022-08-18 16:51:11 +0900

    feat: Form Validation for posting

--- blog/src/pages/Post.js
+++ blog/src/pages/Post.js
@@ -1,17 +1,28 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Post = () => {
-  const [titleInput, setTitleInput] = useState("");
-  const [contentInput, setContentInput] = useState("");
+  const [titleInput, setTitleInput] = useState('');
+  const [contentInput, setContentInput] = useState('');
+  const titleInValid= titleInput.length >4 && titleInput.length < 30;
+  const contentInValid= contentInput.length > 30
+  const titleRef = useRef(null);
+  const contentRef = useRef(null);
+  
   const titleChange = event => {
     setTitleInput(event.target.value)
   }
   const contentChange = event => {
     setContentInput(event.target.value)
   }
-  const publishBtn = () => {
-    console.log("글을 올렸습니다");
+  const publishBtn = (event) => {
+    if(!titleInValid){
+      titleRef.current.focus()
+      event.preventDefault()
+    } else if(!contentInValid) {
+      contentRef.current.focus()
+      event.preventDefault()
+    }
   };
 
   return (
@@ -20,11 +31,18 @@ const Post = () => {
       <form>
         <div>
           <label htmlFor="Title">제목</label>
-          <input id="Title" value={titleInput} onChange={titleChange} type="text" size={30} />
+          <br/>
+          <input id="Title" ref={titleRef} value={titleInput} onChange={titleChange} type="text" size={30} />
+          <br/>
+          {!titleInValid && '제목을 간결하게 작성해주세요.'}
         </div>
+        <br/>
         <div>
           <label htmlFor="Content">내용</label>
-          <input id="Content" value={contentInput} onChange={contentChange} type="text" size={70} />
+          <br/>
+          <input id="Content" ref={contentRef} value={contentInput} onChange={contentChange} type="text" size={70} />
+          <br/>
+          {!contentInValid && '내용을 간결하게 작성해주세요.'}
         </div>
         <div>
           <Link to="/">

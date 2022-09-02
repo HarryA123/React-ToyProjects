2650f5b163d9fe73cdfd309bc2a9c2d2888cc721
harry_kwak <harrykwak123@gmail.com>
2022-09-02 16:55:19 +0900

    chore: remove unused lines

--- news/src/components/Main.js
+++ news/src/components/Main.js
@@ -1,37 +1,17 @@
 import React, { useState } from "react";
-import axios from "axios";
-import { useDispatch  }from 'react-redux';
-import { newsSlice } from "../store/store";
+import { useSelector, useDispatch }from 'react-redux';
 import { Link } from 'react-router-dom';
 import Article from './Article'
+import { getArticle } from "../store/store";
 
 const Main = () => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
   const [value, setValue] = useState('');
+  const articles = useSelector(state=>state.news.articles);
+  const isLoading = useSelector(state=>state.news.isLoading);
+  console.log('articles는 ', articles, 'isLoading은 ',isLoading)
   const dispatch = useDispatch();
-  // const isClipped = useSelector((state)=>state.news.isClipped)
-
-  const api = async (searchKeyword, page=1) => {
-    setLoading(true);
-    try {
-      const response = await axios.get(
-        `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchKeyword}&page=${page}&sort=newest&api-key=${process.env.REACT_APP_API_KEY}`
-      );
-      console.log(response.data.response.docs);
-      setArticles(response.data.response.docs);
-    } catch (e) {
-      console.log(e);
-    }
-    setLoading(false);
-  };
-  // const onClick = ()=>{
-  //   api()
-  //   console.log('click으로 api를 호출했습니다. 잠시만 기다리세요!')
-  // }
 
   const onChange = (e)=>{
-    console.log('타이핑을 하고 있습니다!')
     console.log(e.target.value)
     setValue(e.target.value)
   }
@@ -39,7 +19,7 @@ const Main = () => {
     if(value){
       e.preventDefault()
       console.log('1. preventDefault로 새로고침 막기 성공!')
-      api(value)
+      dispatch(getArticle({value:value, page:1}))
       console.log('2. enter 또는 click을 하셨군요! submit으로 api를 호출했습니다!')
       setValue('')
       console.log('3. 검색창이 reset 되었습니다!')
@@ -60,7 +40,7 @@ const Main = () => {
       <button> click</button>
       </form>
       <Link to={'/clips'}><button>clips📌</button></Link>
-      <h2>{loading ? "뉴스를 불러오고 있습니다📰" : null}</h2>
+      <h2>{isLoading ? "뉴스를 불러오고 있습니다📰" : null}</h2>
       <div>
         {articles && articles.map((ele) => (
           <Article key={ele._id} ele={ele}/>

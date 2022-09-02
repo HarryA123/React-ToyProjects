60a18422e2a347d927e08bfaca037b0486122bbb
harry_kwak <harrykwak123@gmail.com>
2022-09-02 16:47:18 +0900

    feat: Create newsSlice, getArticle Api

--- news/src/store/store.js
+++ news/src/store/store.js
@@ -1,32 +1,27 @@
 // import { createStore, combineReducers} from 'redux'
 // import ReduxThunk from 'redux-thunk';
-import {configureStore , createSlice} from '@reduxjs/toolkit'
+import {configureStore , createSlice, applyMiddleware, createAsyncThunk} from '@reduxjs/toolkit';
+import axios from "axios";
 
-const initialState = {
-  isLoading: false,
-  clipped:[],
-  unClipped:[],
-  articles:[],
-  searchHistory:[]
-}
-
-export const newsSlice = createSlice({
-  name : 'newsSlice',
-  initialState,
-  reducers:{
-    clip :(state)=>{
-      console.log('clip되었음!')
-      state.clipped = state.clipped.push()
-    }
+export const getArticle = createAsyncThunk('newsSlice/getArticle', async ({value, page})=>{
+    console.log('내 검색 : ',value, '& 페이지 : ',page)
+    const response = await axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${value}&page=${page}&sort=newest&api-key=${process.env.REACT_APP_API_KEY}`);
+    console.log('api가 여기까지 호출됐어요!',response.data.response.docs)
+    return response.data.response.docs
   }
-})
+)
 
-const store = configureStore({
-  reducer : {
-    news: newsSlice.reducer
-  }
+// export const api = async (value, page) => {
+//   try {
+//     console.log('value: ',value, '& page: ',page)
+//     const response = await axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${value}&page=${page}&sort=newest&api-key=${process.env.REACT_APP_API_KEY}`);
+//     console.log(response.data.response.docs);
+//     return response.data.response.docs;
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
 
-})
 
 // const api = async (searchKeyword) => {
 //   setLoading(true);

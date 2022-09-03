1438e0685d6b862e7965b2f9f5637ebb0f0bb1f7
harry_kwak <harrykwak123@gmail.com>
2022-09-04 00:40:47 +0900

    feat: Add Clip icon feature (on & off)

--- news/src/components/Article.js
+++ news/src/components/Article.js
@@ -1,22 +1,26 @@
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { newsSlice } from "../store/store";
+import { ClipIcon } from "./style";
+
 
 const Article = ({ ele }) => {
   const dispatch = useDispatch();
-
+  const clips = useSelector(state => state.news.clips)
+  console.log(clips)
   return (
-    <div>
-      <span onClick={() => {
-        console.log('articles 중 이 뉴스 ', ele)
+    <>
+      <ClipIcon onClick={() => {
+        console.log('➡ articles 중 이 뉴스를 요청했습니다. ', ele)
         dispatch(newsSlice.actions.clip(ele))}}>
-        📌
-      </span>
+        {clips.includes(ele) ? '🔵' :'⚪' }
+        {/* {만약 clip 배열에 기사가 포함되어 있으면 🔵, 배열에 없으면 ⚪ } */}
+      </ClipIcon>
       <a href={ele.web_url}>{ele.headline.main}</a>
       <p>{ele.snippet}</p>
       <p>{ele.pub_date.slice(0, 10).replaceAll("-", ".")}</p>
       <br />
       <br />
-    </div>
+    </>
   );
 };
 
--- news/src/components/style.js
+++ news/src/components/style.js
@@ -1,7 +1,19 @@
 import styled from "styled-components";
 
-export const HistoryList = styled.div`
+export const HistoryBox = styled.div`
   width: 170px;
   height: 116px;
   background-color: gray;
 `
+export const HistoryList = styled.div`
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  padding: 0 0.4rem 0;
+`
+export const ClipIcon = styled.span`
+  cursor: pointer;
+`
+
+
+

4c5a4d06fe65e9ac6b32c08647172f55835108ec
harry_kwak <harrykwak123@gmail.com>
2022-09-04 00:41:51 +0900

    style: Add Keyword History List style

--- news/src/components/Main.js
+++ news/src/components/Main.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import Article from "./Article";
 import { getArticle, newsSlice } from "../store/store";
-import { HistoryList } from "./style";
+import { HistoryList, HistoryBox } from "./style";
 
 const Main = () => {
   const [value, setValue] = useState("");
@@ -11,36 +11,25 @@ const Main = () => {
   const isLoading = useSelector((state) => state.news.isLoading);
   const searchHistory = useSelector((state) => state.news.searchHistory);
   // const searchHistory = useSelector(state=>state.news.searchHistory);
-  console.log("articles는 ", articles, "isLoading은 ", isLoading);
   const dispatch = useDispatch();
-
+  
   const onChange = (e) => {
     console.log(e.target.value);
     setValue(e.target.value);
-    // const booGi = setTimeout(() => {
-    //   dispatch(getArticle({value:value, page:1}))
-    //   clearTimeout(booGi)
-    // }, 500);
-    // booGi()
   };
   const onSubmit = (e) => {
+    console.log("articles는 ", articles, "isLoading은 ", isLoading);
     if (value) {
       e.preventDefault();
-      console.log("1. preventDefault로 새로고침 막기 성공!");
       dispatch(getArticle({ value: value, page: 1 }));
-      console.log(
-        "2. ➡enter 또는 click을 하셨군요! submit으로 api를 호출했습니다!"
-      );
       if (searchHistory.includes(value)) {
         dispatch(newsSlice.actions.history(value));
       } else {
         dispatch(newsSlice.actions.historyUpdate(value));
       }
       setValue("");
-      console.log("3. 검색창이 reset 되었습니다!");
     } else {
       e.preventDefault();
-      console.log("1. 검색창이 비었어요. ");
     }
   };
 
@@ -51,15 +40,14 @@ const Main = () => {
         <input value={value} onChange={onChange} />
         <button> click</button>
       </form>
-      <HistoryList>
+      <HistoryBox>
         {searchHistory &&
           [...searchHistory]
             .reverse()
             .map((ele) => {
-            return <div key={ele}>{ele}</div>})}
-      </HistoryList>
-      <button>clips📌</button>
-      <Link to={"/clips"}></Link>
+            return <HistoryList key={ele}>{ele}</HistoryList>})}
+      </HistoryBox>
+      <Link to={"/clips"}><button>clips📌</button></Link>
       <h2>{isLoading ? "뉴스를 불러오고 있습니다📰" : null}</h2>
       <div>
         {articles && articles.map((ele) => <Article key={ele._id} ele={ele} />)}

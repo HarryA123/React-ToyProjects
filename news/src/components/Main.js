4f4f8d989c17432595efdb7f98b956fb1e86284a
harry_kwak <harrykwak123@gmail.com>
2022-09-04 16:52:44 +0900

    feat: setTimeout Submit after 2 seconds

--- news/src/components/Main.js
+++ news/src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import Article from "./Article";
@@ -12,13 +12,55 @@ const Main = () => {
   const searchHistory = useSelector((state) => state.news.searchHistory);
   // const searchHistory = useSelector(state=>state.news.searchHistory);
   const dispatch = useDispatch();
+  const timer = useRef(null)
   
+
+  // 타이핑하고 2초통안 움직이지 않으면(2초 후) submit이 발생하고,
+  // submit 발생하면서 api호출해 기사를 불러오고,
+  // 검색키워드를 store로 보내 히스토리 배열에 넣는다. 
+
+
+// 타이핑을 치면 2초 뒤에 서밋이 된다.
+
   const onChange = (e) => {
+    console.log('timer는...........',timer)
     console.log(e.target.value);
+    console.log(e);
     setValue(e.target.value);
+    console.log(e.target.value)
+
+    console.log(!e.target.value)
+    if(e.target.value){
+      console.log('2초 뒤 서밋 시작')
+      clearTimeout(timer.current)
+      timer.current = setTimeout(()=>{
+        if (e.target.value) {
+          //이벤트 타이핑을 했을 경우.
+          e.preventDefault();
+          dispatch(getArticle({ value: e.target.value, page: 1 }));
+          if (searchHistory.includes(e.target.value)) {
+            dispatch(newsSlice.actions.history(e.target.value));
+          } else {
+            dispatch(newsSlice.actions.historyUpdate(e.target.value));
+          }
+          setValue("");
+        } else {
+          //이벤트 타이핑을 하지 않았을 경우.
+          e.preventDefault();
+        }
+      }, 2000)
+      // 만약 e.target.value가 없으면, 2초 뒤 서밋.
+    } else{
+      console.log(typeof(value))
+      console.log('음...')
+    }
+
   };
   const onSubmit = (e) => {
     console.log("articles는 ", articles, "isLoading은 ", isLoading);
+    console.log(e.target.value)
+    // const value = e.target.value;
+    // console.log(value)
     if (value) {
       e.preventDefault();
       dispatch(getArticle({ value: value, page: 1 }));
@@ -37,8 +79,8 @@ const Main = () => {
     <>
       <h1>🌏New World News🌍</h1>
       <form onSubmit={onSubmit}>
-        <input value={value} onChange={onChange} />
-        <button> click</button>
+        <input autoFocus type = 'text' autoComplete='on' value={value} onChange={onChange} />
+        <button type='submit'> click</button>
       </form>
       <HistoryBox>
         {searchHistory &&

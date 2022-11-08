cebc165a9cb5e877b944c787ffbd6a1fec061ab6
HarryA123 <harrykwak123@gmail.com>
2022-11-08 23:25:11 +0900

    feat: Create infinite scroll

--- movie-app/src/pages/Home.js
+++ movie-app/src/pages/Home.js
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import { useEffect, useRef, useState } from "react";
 import Movie from "../components/Movie";
 import HeaderComponent from "../components/HomeButton";
 import GlobalStyle from "../GlobalStyle";
+import { Route, useParams } from "react-router-dom";
 import { LoadingStyle, ListContainer } from "../components/styles";
 
 function Home() {
@@ -9,11 +11,15 @@ function Home() {
   const [movies, setMovies] = useState([]);
   const [movieSearch, setMovieSearch] = useState("");
   const [movieName, setMovieName] = useState("");
-  console.log(loading);
+  const [pageNumber, setPageNumber] = useState(1);
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+  const param = useParams();
   const getMovies = async () => {
     const json = await (
       await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=1&page=&query_term=${movieName}&sort_by=year`
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=1&page=${pageNumber}&query_term=${movieName}&sort_by=year`
       )
     ).json();
     setMovies(json.data.movies);
@@ -26,13 +32,27 @@ function Home() {
 
   const onSubmit = event => {
     event.preventDefault();
-    setMovieName(movieSearch);
-    getMovies();
+    if (typeof param === Object) {
+      setMovieName(movieSearch);
+      getMovies();
+    } else {
+      Route(`/main`);
+    }
   };
 
+  // When User Searching...
   useEffect(() => {
     getMovies();
   }, [movieName]);
+
+  // When User Scroll, Keep Adding Movies at the bottom...
+  useEffect(() => {
+    setPageNumber(prev => prev + 1);
+    setMovies(prev => {
+      return [...movies, ...prev];
+    });
+    getMovies();
+  }, [inView]);
 
   return (
     <>
@@ -45,23 +65,27 @@ function Home() {
       {loading ? (
         <LoadingStyle>Loading...</LoadingStyle>
       ) : (
-        <ListContainer>
-          {movies.map(item => {
-            return (
-              <Movie
-                key={item.id}
-                id={item.id}
-                title={item.title}
-                year={item.year}
-                medium_cover_image={item.medium_cover_image}
-                rating={item.rating}
-                runtime={item.runtime}
-                genres={item.genres}
-                summary={item.summary}
-              />
-            );
-          })}
-        </ListContainer>
+        <>
+          <ListContainer>
+            {movies.map(item => {
+              return (
+                <Movie
+                  key={item.title}
+                  id={item.id}
+                  title={item.title}
+                  year={item.year}
+                  medium_cover_image={item.medium_cover_image}
+                  rating={item.rating}
+                  runtime={item.runtime}
+                  genres={item.genres}
+                  summary={item.summary}
+                />
+              );
+            })}
+          </ListContainer>
+          {inView ? <>🎭</> : <>🧶</>}
+          <div ref={ref} style={{ width: "100%", height: "20px" }}></div>
+        </>
       )}
     </>
   );

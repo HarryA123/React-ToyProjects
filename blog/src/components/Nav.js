340665c700b59995a8d63ad05b5b19aede8b57fe
HarryA123 <harrykwak123@gmail.com>
2022-11-19 23:38:10 +0900

    chore: import styles

--- blog/src/components/Nav.js
+++ blog/src/components/Nav.js
@@ -1,22 +1,14 @@
 import React from "react";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-
-const LinkS = styled(Link)`
-  padding: 12px 80px;
-  position: fixed;
-  top: 0;
-  width: 100vh;
-  background: -webkit-linear-gradient(#ec02a1, #00d4ff);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  background-color: gray;
-  font-size: 20px;
-  font-weight: bold;
-`;
+// import Button from "./buttons/Button";
+import { LinkS, Navigation, NavButton } from "./style";
 
 const Nav = () => {
-  return <LinkS to="/">Blog</LinkS>;
+  return (
+    <Navigation>
+      <LinkS to="/">Blog</LinkS>
+      <NavButton navButtonName="글쓰기" />
+    </Navigation>
+  );
 };
 
 export default Nav;

6130f3c86e38fdbea786321f54a3b74445b54c2d
HarryA123 <harrykwak123@gmail.com>
2022-11-21 21:22:30 +0900

    feat: Apply RTK useDispatch for Clip

--- movie-app/src/components/Movie.js
+++ movie-app/src/components/Movie.js
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import { Container, Title, Poster, FlexColumn, Info, FlexRow } from "./styles";
+import movieSlice from "../features/movieSlice";
 
 function Movie({
   id,
@@ -10,6 +13,14 @@ function Movie({
   runtime,
   genres,
 }) {
+  const state = useSelector(state => state.reducer.clips);
+  const dispatch = useDispatch();
+  const getClip = () => {
+    dispatch({ type: "clip/clip", title });
+    // dispatch(movieSlice.actions.clip(title));
+  };
+  console.log(state);
+
   return (
     <Container>
       <FlexRow>
@@ -20,6 +31,7 @@ function Movie({
           <Title>
             {title} ({year})
           </Title>
+          <div onClick={getClip}>{state.includes(title) ? "💜" : "🤍"}</div>
           <Info>
             Rating : {rating} <br /> RunTime : {(runtime / 60).toFixed(1)}h
             <br />

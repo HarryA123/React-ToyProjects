81a178bc37f9f19ae55062850f03a3cd892459e9
HarryA123 <harrykwak123@gmail.com>
2022-11-25 17:13:07 +0900

    feat: Add Not Exist Movie JSX

--- movie-app/src/pages/Home.js
+++ movie-app/src/pages/Home.js
@@ -1,10 +1,11 @@
 import { useInView } from "react-intersection-observer";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
 import HeaderComponent from "../components/HomeButton";
 import GlobalStyle from "../GlobalStyle";
-import { Route, useParams } from "react-router-dom";
-import { LoadingStyle, ListContainer } from "../components/styles";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { LoadingStyle, FindError, ListContainer } from "../components/styles";
 
 function Home() {
   const [loading, setLoading] = useState(true);
@@ -12,10 +13,15 @@ function Home() {
   const [movieSearch, setMovieSearch] = useState("");
   const [movieName, setMovieName] = useState("");
   const [pageNumber, setPageNumber] = useState(1);
-  const { ref, inView } = useInView({
+  const { ref, inView, entry } = useInView({
     threshold: 0,
   });
+  // console.log(ref, inView, entry);
+  // console.log(movies);
   const param = useParams();
+  console.log("param:", param);
+  console.log(movieName);
+  console.log("🌭검색결과들...", movies);
   const getMovies = async () => {
     const json = await (
       await fetch(
@@ -32,27 +38,38 @@ function Home() {
 
   const onSubmit = event => {
     event.preventDefault();
-    if (typeof param === Object) {
+    if (movieName.trim() === "") {
+      alert("키워드를 입력하세요");
+    } else {
       setMovieName(movieSearch);
       getMovies();
-    } else {
-      Route(`/main`);
     }
   };
+  const clips = useSelector(state => state.reducer.clips);
+  console.log(clips);
 
   // When User Searching...
   useEffect(() => {
     getMovies();
   }, [movieName]);
 
+  // if (inView === true) {
+  //   setPageNumber(prev => prev + 1);
+  //   setMovies(prev => {
+  //     return prev.concat(movies);
+  //   });
+  //   getMovies();
+  // } else {
+
+  // }
   // When User Scroll, Keep Adding Movies at the bottom...
-  useEffect(() => {
-    setPageNumber(prev => prev + 1);
-    setMovies(prev => {
-      return [...movies, ...prev];
-    });
-    getMovies();
-  }, [inView]);
+  // useEffect(() => {
+  //   setPageNumber(prev => prev + 1);
+  //   setMovies(prev => {
+  //     return prev.concat(movies);
+  //   });
+  //   getMovies();
+  // }, [inView]);
 
   return (
     <>
@@ -66,23 +83,37 @@ function Home() {
         <LoadingStyle>Loading...</LoadingStyle>
       ) : (
         <>
-          <ListContainer>
-            {movies.map(item => {
-              return (
-                <Movie
-                  key={item.title}
-                  id={item.id}
-                  title={item.title}
-                  year={item.year}
-                  medium_cover_image={item.medium_cover_image}
-                  rating={item.rating}
-                  runtime={item.runtime}
-                  genres={item.genres}
-                  summary={item.summary}
-                />
-              );
-            })}
-          </ListContainer>
+          {movies ? (
+            <ListContainer>
+              {movies.map(item => {
+                return (
+                  <Movie
+                    key={item.title}
+                    id={item.id}
+                    title={item.title}
+                    year={item.year}
+                    medium_cover_image={item.medium_cover_image}
+                    rating={item.rating}
+                    runtime={item.runtime}
+                    genres={item.genres}
+                    summary={item.summary}
+                  />
+                );
+              })}
+            </ListContainer>
+          ) : (
+            <FindError>
+              <h4
+                style={{
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                  whiteSpace: "nowrap",
+                }}>
+                {movieName}
+              </h4>
+              이 영화를 찾을 수 없습니다
+            </FindError>
+          )}
           {inView ? <>🎭</> : <>🧶</>}
           <div ref={ref} style={{ width: "100%", height: "20px" }}></div>
         </>

0f4debb7972a9de39a196805944ec5788ce1870b
HarryA123 <harrykwak123@gmail.com>
2022-11-26 19:27:41 +0900

    feat: Add Reducer Slice

--- movie-app/src/store.js
+++ movie-app/src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import Slices from "./features/Slices";
+import movieSlice from "./features/movieSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: movieSlice,
 });

bb66307045c316c25429d1f21c14a4ddde0d915d
HarryA123 <harrykwak123@gmail.com>
2022-12-09 16:21:37 +0900

    feat: import scss file from Styles

--- news/src/App.js
+++ news/src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import Main from "./components/Main";
-import Clip from "./components/Clip";
+import Main from "./Pages/Main";
+import Clip from "./Pages/Clip";
 import NotFound from "./components/NotFound";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />}></Route>
-          <Route path="/clips" element={<Clip />}></Route>
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/clips" element={<Clip />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 

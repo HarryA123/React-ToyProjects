1221c847ffcf00cb357b3f51aa067e911abb411a
HarryA123 <harrykwak123@gmail.com>
2022-12-09 16:20:24 +0900

    feat: Add scss files

--- news/src/components/Article.js
+++ news/src/components/Article.js
@@ -1,26 +1,25 @@
+import "../Styles/Article.css";
 import { useSelector, useDispatch } from "react-redux";
 import { newsSlice } from "../store/store";
-import { ClipIcon } from "./style";
-
 
 const Article = ({ ele }) => {
   const dispatch = useDispatch();
-  const clips = useSelector(state => state.clips)
+  const clips = useSelector(state => state.clips);
   // console.log(clips)
   return (
-    <>
-      <ClipIcon onClick={() => {
-        console.log('➡ articles 중 이 뉴스를 요청했습니다. ', ele)
-        dispatch(newsSlice.actions.clip(ele))}}>
-        {clips.includes(ele) ? '🔵' :'⚪' }
+    <div className="news_Card">
+      <span className="ClipIcon"
+        onClick={() => {
+          console.log("➡ articles 중 이 뉴스를 요청했습니다. ", ele);
+          dispatch(newsSlice.actions.clip(ele));
+        }}>
+        {clips.includes(ele) ? "🔵" : "⚪"}
         {/* {만약 clip 배열에 기사가 포함되어 있으면 🔵, 배열에 없으면 ⚪ } */}
-      </ClipIcon>
-      <a href={ele.web_url}>{ele.headline.main}</a>
-      <p>{ele.snippet}</p>
-      <p>{ele.pub_date.slice(0, 10).replaceAll("-", ".")}</p>
-      <br />
-      <br />
-    </>
+      </span>
+      <a className="news_Headline" href={ele.web_url}>{ele.headline.main}</a>
+      <div className="news_Snippet" >{ele.snippet}</div>
+      <div className="news_Date" >{ele.pub_date.slice(0, 10).replaceAll("-", ".")}</div>
+    </div>
   );
 };
 

7ae759b26771100eaf718eac1c27c075d6dcd6d6
HarryA123 <harrykwak123@gmail.com>
2022-12-12 12:38:02 +0900

    feat: Create Clip scss files

--- news/src/Pages/Clip.js
+++ news/src/Pages/Clip.js
@@ -1,23 +1,25 @@
-import { Link } from 'react-router-dom';
-import { useSelector  }from 'react-redux';
-import Article from '../components/Article';
+import { useSelector } from "react-redux";
+import Article from "../components/Article";
+import NavBar from "../components/NavBar";
+import "../Styles/Clip.css";
 
 const Clip = () => {
-  const clips = useSelector(state=>state.clips)
-  console.log('clipped List: ',typeof(clips))
+  const clips = useSelector(state => state.clips);
+  console.log("clipped List: ", typeof clips);
 
   return (
-  <>
-    <h1>📌My Clips📌</h1>
+    <>
+      <NavBar />
+      <div className="PageTitle">My Clips</div>
+      <div className="Article_Container">
+        {clips.length === 0 ? (
+          <h4>There is no clipped article</h4>
+        ) : (
+          clips.map(ele => <Article key={ele._id} ele={ele} />)
+        )}
+      </div>
+    </>
+  );
+};
 
-    <Link to={'/'}><button>home🌍</button></Link>
-    <div>
-      {clips.length === 0 ? <h4>There is no clipped article</h4> : clips.map((ele) => (
-        <Article key={ele._id} ele={ele}/>
-      ))}
-    </div>
-  </>
-  ) 
-}
-
-export default Clip
+export default Clip;

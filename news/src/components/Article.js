bc8108ee713649c9768338d9bfbcd38e24b1ba36
HarryA123 <harrykwak123@gmail.com>
2022-12-14 12:03:28 +0900

    chore: prettier

--- news/src/components/Article.js
+++ news/src/components/Article.js
@@ -7,15 +7,20 @@ const Article = ({ ele }) => {
   const clips = useSelector(state => state.clips);
   return (
     <div className="news_Card">
-      <span className="ClipIcon"
+      <span
+        className="ClipIcon"
         onClick={() => {
           dispatch(newsSlice.actions.clip(ele));
         }}>
         {clips.includes(ele) ? "🔵" : "⚪"}
       </span>
-      <a className="news_Headline" href={ele.web_url}>{ele.headline.main}</a>
-      <div className="news_Snippet" >{ele.snippet}</div>
-      <div className="news_Date" >{ele.pub_date.slice(0, 10).replaceAll("-", ".")}</div>
+      <a className="news_Headline" href={ele.web_url}>
+        {ele.headline.main}
+      </a>
+      <div className="news_Snippet">{ele.snippet}</div>
+      <div className="news_Date">
+        {ele.pub_date.slice(0, 10).replaceAll("-", ".")}
+      </div>
     </div>
   );
 };

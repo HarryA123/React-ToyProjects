bc0f03b382387796cb423f9f7a8da7b8c5172805
HarryA123 <harrykwak123@gmail.com>
2023-01-11 22:36:23 +0900

    feat: Register After Reset User Info

--- movie-app/src/features/userSlice.js
+++ movie-app/src/features/userSlice.js
@@ -5,8 +5,12 @@ export const userSlice = createSlice({
   initialState: { user: [], isLogin: false },
   reducers: {
     registerUserInfo: (state, action) => {
-      state.user = [];
-      state.user.push(action.info);
+      if (state.user.length === 0) {
+        state.user.push(action.info);
+      } else {
+        state.user = [];
+        state.user.push(action.info);
+      }
     },
     loginState: (state, action) => {
       if (action.isLogin === true) {

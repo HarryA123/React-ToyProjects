efcde53199b380fa47366fa39125f7b01798cd1e
HarryA123 <harrykwak123@gmail.com>
2023-01-14 03:35:48 +0900

    feat: Add ReduxPersist(SessionStorage)

--- movie-app/src/index.jsx
+++ movie-app/src/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { store } from "./store";
 import { Provider } from "react-redux";
 import App from "./App";
+import { persistor, store } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- movie-app/src/store.js
+++ movie-app/src/store.js
@@ -1,7 +1,56 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import movieSlice from "./features/movieSlice";
 import userSlice from "./features/userSlice";
+import storage from "redux-persist/lib/storage";
+import sessionStorage from "redux-persist/es/storage/session";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+
+
+const rootPersistConfig = {
+  key: "root",
+  storage,
+};
+
+const userPersistConfig = {
+  key: "user",
+  storage: sessionStorage,
+};
+
+const rootReducer = combineReducers({
+  movie: movieSlice.reducer,
+  user: persistReducer(userPersistConfig, userSlice.reducer),
+});
+
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { movie: movieSlice.reducer, user: userSlice.reducer },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import movieSlice from "./features/movieSlice";
+// import userSlice from "./features/userSlice";
+
+// export const store = configureStore({
+//   reducer: {
+// movie: movieSlice.reducer,
+// user: userSlice.reducer
+// },
+// });
